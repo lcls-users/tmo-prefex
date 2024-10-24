fc52885aecabfef9afb16e36e8b91a2fd1e28888
David M. Rogers <predictivestatmech@gmail.com>
2024-10-24 11:41:47 -0400

    Added stream_utils.variable_chunks()

--- src/stream_utils.py
+++ src/stream_utils.py
@@ -1,7 +1,7 @@
 import itertools
 from collections.abc import Iterator
 
-from stream import Stream, Source, stream
+from stream import Stream, Source, stream, take
 
 @stream
 def xmap(items, fn, *args, **kws):
@@ -29,17 +29,68 @@ def split(*streams : Stream):
         return Source( zip(*its) )
     return Stream(fn)
 
+@stream
+def variable_chunks(iterator, sizes: Iterator[int]):
+    """Yield lists of items from the iterator with variable
+    sizes.  If sizes reaches its end, this stream ends.
+    
+    If you want an infinite stream, use something like:
 
-if __name__=="__main__":
+    >>> sizes = stream.Source([10, 20, 100]) << stream.seq(1000,0)
+
+    which will stick at 1000 forever.
+    """
+    n = 0
+    for n in sizes:
+        s = iterator >> take(n) >> list
+        if s:
+            yield s
+        else:
+            return
+    while n > 0:
+        s = iterator >> take(n) >> list
+        if s:
+            yield s
+        else:
+            return
+
+def test_split():
     import stream
+
+    state = [] # track execution of src with this
     def src():
         for i in range(4):
             print(f"at {i}")
+            state.append(i)
             yield i
     s = src()
     s >>= stream.map(lambda x: x)
+    j = 0
     for i in s >> split(stream.map(lambda x: 'a'),
                         stream.map(lambda x: x+1),
                         stream.map(lambda x: 2*x)):
-        print(i)
+        #print(i)
+        assert tuple(state) == tuple(range(j+1))
+        assert i == ('a', j+1, j*2)
+        j += 1
 
+def test_variable():
+    import stream
+    expander = variable_chunks([2, 5, 10])
+    for i, v in enumerate(stream.seq() >> expander >> take(4)):
+        print(v)
+        assert isinstance(v, list)
+        if i == 0:
+            assert tuple(v) == tuple(range(2))
+        elif i == 1:
+            assert tuple(v) == tuple(range(2, 2+5))
+        elif i == 2:
+            assert tuple(v) == tuple(range(2+5, 2+5+10))
+        elif i == 3:
+            assert tuple(v) == tuple(range(2+5+10, 2+5+10*2))
+        else:
+            assert False
+
+if __name__=="__main__":
+    #test_split()
+    test_variable()
