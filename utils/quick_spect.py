83d6b7384477c03c874ef769e8d53d073da9d9d9
Ryan N. Coffee <coffee@slac.stanford.edu>
2024-10-30 22:57:37 -0700

    utils/quick_spect.py

--- utils/quick_spect.py
+++ utils/quick_spect.py
@@ -8,27 +8,38 @@ import re
 
 def main():
     fname = sys.argv[1]
+    hist = {}
+    maxtof:np.uint32 = 1<<14
     with h5py.File(fname,'r') as f:
         runstr = [k for k in f.keys()][0]
         detstr = 'mrco_hsd'
-        ports = f[runstr][detstr].keys()
-        portnums = np.sort([int(re.search('_(\d+)$',k).group(1)) for k in ports])
-        for p in portnums:
-            t = f[runstr][detstr]['port_%i'%p]['tofs'][()]
-            tvalid = [int(v)>>1 for v in t if v<(1<<14)]
-            h=np.zeros((1<<int(np.log2(np.max(tvalid)))+1))
+        hsd = f[runstr][detstr]
+        ports = [p for p in hsd.keys()]
+        #portnums = np.sort([int(re.search('_(\d+)$',k).group(1)) for k in ports])
+        portnums = [int(re.search('_(\d+)$',k).group(1)) for k in ports]
+        print("portnums:\t",portnums)
+        for p in ports:
+            t = hsd[p]['tofs'][()]
+            tvalid = [v for v in t if v<maxtof]
+            hist.update({p:[0]*maxtof})
             for v in tvalid:
-                h[v] += 1
-            plt.title('port_%i'%p)
-            plt.plot(h,label='port_%i'%p)
-            plt.legend()
-            plt.xlim(2800,3500)
-            plt.xlabel('histbins = 2 hsd bins')
-            plt.ylabel('hits')
-            plt.show()
+                hist[p][v] += 1
+
+    fig,axs = plt.subplots(nrows=4, ncols=4, figsize=(25, 24))
+    for i,p in enumerate(hist.keys()):
+        col = i%4
+        row = i>>2
+        axs[row,col].stairs(hist[p])
+        axs[row,col].set_title(p)
+        axs[row,col].set_xlabel('tof bins * expand')
+        axs[row,col].set_ylabel('counts')
+    plt.show()
+
 
 if __name__ == "__main__":
     if len(sys.argv)>1:
         main()
     else:
         print('point me to an .h5 file!')
+
+
