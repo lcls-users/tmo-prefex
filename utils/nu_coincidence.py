0521b1a8f3f8b91f15941ee306b6210d55ed21c3
Ryan Coffee <coffeer76@gmail.com>
2024-11-05 14:54:12 -0800

    no addresses chunks 1 on

--- utils/nu_coincidence.py
+++ utils/nu_coincidence.py
@@ -11,7 +11,7 @@ import statistics as stats
 def main():
     path = sys.argv[1]
     filenames = [nm for nm in os.listdir(path) if os.path.isfile(os.path.join(path, nm))]
-    hist = np.zeros((1<<8,1<<8),dtype=np.uint64)
+    hist = np.zeros((1<<9,1<<9),dtype=np.uint64)
     tofs = {}
     addresses = {}
     nedges = {}
@@ -32,17 +32,23 @@ def main():
                 tofs.update({p:hsd['port_180']['tofs'][()]})
                 addresses.update({p:hsd[p]['addresses'][()]})
                 nedges.update({p:hsd[p]['nedges'][()]})
-            for i,a1 in enumerate(addresses['port_000']):
+            for i in range(len(addresses['port_000'])):
                 n1 = nedges['port_000'][i]
                 n2 = nedges['port_180'][i]
+                a1 = addresses['port_000'][i]
                 a2 = addresses['port_180'][i]
                 for t1 in tofs['port_000'][a1:a1+n1]:
                     for t2 in tofs['port_180'][a2:a2+n2]:
-                        ind1 = max(0,min(hist[p].shape[0]-1, int(t1-t0)>>5))
-                        ind2 = max(0,min(hist[p].shape[0]-1, int(t2-t0)>>5)
-                        h[ind1,ind2] += 1
+                        ind1 = max(0,min(hist.shape[0]-1, int(t1-t0)>>1))
+                        ind2 = max(0,min(hist.shape[1]-1, int(t2-t0)>>1))
+                        hist[ind1,ind2] += 1
 
 
+    plt.imshow(hist,clim=(0,5))
+    plt.colorbar()
+    plt.show()
+
+    '''
     fig,axs = plt.subplots(nrows=4, ncols=4, figsize=(25, 24))
     for i,p in enumerate(hist.keys()):
         col = i%4
@@ -50,6 +56,7 @@ def main():
         axs[row,col].imshow(hist[p]) 
         axs[row,col].set_title(str(np.sum(hist[p]))) 
     plt.show()
+    '''
 
 
 if __name__ == "__main__":
