Bugfix: incorrect offset computation for Hsd and Spect detectors.

tmo_prefex/Hsd.py
@@ -16,7 +16,8 @@ from stream import stream
 from .utils import (
     mypoly, tanhInt, tanhFloat,
     randomround, quick_mean, concat,
-    cfdLogic, fftLogic_f16, fftLogic_fex, fftLogic
+    cfdLogic, fftLogic_f16, fftLogic_fex, fftLogic,
+    calc_offsets
 )
 
 _rng = np.random.default_rng( time.time_ns()%(1<<8) )
@@ -436,7 +437,7 @@ def save_hsd(waves: Union[List[WaveData], List[FexData]]
     if len(nedges) == 0:
         addresses = np.array([], dtype=np.uint64)
     else:
-        addresses = nedges.cumsum()-nedges[0]
+        addresses = calc_offsets(nedges)
     return dict(
         config = waves[0].cfg,
         events = np.array(events, dtype=np.uint32),

tmo_prefex/Spect.py
@@ -6,7 +6,7 @@ from stream import stream
 
 import numpy as np
 
-from .utils import concat
+from .utils import concat, calc_offsets
 
 def getCentroid(data,pct=.8):
     csum = np.cumsum(data.astype(float))
@@ -139,7 +139,6 @@ def save_spect(data: List[SpectData]) -> Dict[str,Any]:
         vsize = np.array([x.vsize for x in d1], dtype=np.int32),
     )
     if data[0].cfg.save_wv: # capturing raw x.v
-        ans['offsets'] = ans['vsize'].astype('uint64').cumsum() \
-                         - ans['vsize'][0]
+        ans['offsets'] = calc_offsets(ans['vsize'])
         ans['wv'] = concat([x.v for x in d1])
     return ans

tmo_prefex/combine.py
@@ -5,7 +5,7 @@ from .Config import DetectorConfig
 from .Hsd import HsdConfig, WaveData, FexData
 from .Gmd import GmdConfig, GmdData
 from .Spect import SpectConfig, SpectData # was Vls?
-from .utils import concat
+from .utils import concat, calc_offsets
 
 import numpy as np
 from stream import filter
@@ -89,7 +89,7 @@ class Batch(dict):
                     if tuple(sz) != tuple(sz2):
                         raise ValueError(f"Length mismatch between keys {sz_key} and {sz_key2}: {sz} vs. {sz2}")
 
-                off = np.cumsum([0] + sz[:-1])
+                off = calc_offsets(sz)
                 u = [v] + [d[idx][k] for d in data]
                 for x, o in zip(u, off):
                     x += o