6ad0c6f54fd8d2fe1b87d7e098444a6246476ef3
Ryan Coffee <coffeer76@gmail.com>
2024-11-11 11:48:43 -0800

    coincidence

--- utils/nu_coincidence.py
+++ utils/nu_coincidence.py
@@ -11,12 +11,14 @@ import statistics as stats
 def main():
     path = sys.argv[1]
     filenames = [nm for nm in os.listdir(path) if os.path.isfile(os.path.join(path, nm))]
-    hist = np.zeros((1<<9,1<<9),dtype=np.uint64)
+    hist = np.zeros((1<<10,1<<10),dtype=np.uint64)
+    h = {}
     tofs = {}
     addresses = {}
     nedges = {}
     maxtof:np.uint32 = 1<<15
     t0=11500
+    scale = 1
     for name in filenames:
         with h5py.File(os.path.join(path,name),'r') as f:
             runstr = [k for k in f.keys()][0]
@@ -28,23 +30,36 @@ def main():
             ens = xgmd['energy'][()]
             cents = fzp['centroids'][()]
         
-            for p in ['port_000','port_180']:
-                tofs.update({p:hsd['port_180']['tofs'][()]})
+            corrports = ['port_000','port_180']
+
+            h.update({corrports[0]:[int(0)]*(hist.shape[0])})
+            h.update({corrports[1]:[int(0)]*(hist.shape[1])})
+
+            for p in corrports:
+                tofs.update({p:hsd[p]['tofs'][()]})
                 addresses.update({p:hsd[p]['addresses'][()]})
                 nedges.update({p:hsd[p]['nedges'][()]})
-            for i in range(len(addresses['port_000'])):
-                n1 = nedges['port_000'][i]
-                n2 = nedges['port_180'][i]
-                a1 = addresses['port_000'][i]
-                a2 = addresses['port_180'][i]
-                for t1 in tofs['port_000'][a1:a1+n1]:
-                    for t2 in tofs['port_180'][a2:a2+n2]:
-                        ind1 = max(0,min(hist.shape[0]-1, int(t1-t0)>>1))
-                        ind2 = max(0,min(hist.shape[1]-1, int(t2-t0)>>1))
+                for t in tofs[p]:
+                    h[p][ max(0,min(len(h[p])-1, int(t-t0)))] +=1
+            scale = len(addresses[corrports[0]])
+            for i in range(len(addresses[corrports[0]])):
+                n1 = nedges[corrports[0]][i]
+                n2 = nedges[corrports[1]][i]
+                a1 = addresses[corrports[0]][i]
+                a2 = addresses[corrports[1]][i]
+                for t1 in tofs[corrports[0]][a1:a1+n1]:
+                    for t2 in tofs[corrports[1]][a2:a2+n2]:
+                        ind1 = max(0,min(hist.shape[0]-1, int(t1-t0)))
+                        ind2 = max(0,min(hist.shape[1]-1, int(t2-t0)))
                         hist[ind1,ind2] += 1
+            #falsecoince = np.outer(h[corrports[0]],h[corrports[1]]).astype(float)
+            #falsecoince /= float(scale>>4)
 
 
-    plt.imshow(hist,clim=(0,5))
+    #plt.imshow(falsecoince,clim=(0,10))
+    #plt.colorbar()
+    #plt.show()
+    plt.imshow(hist,clim=(0,10))
     plt.colorbar()
     plt.show()
 
