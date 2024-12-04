d3ee464bc8b045b13d1d7bb613ddda94809772e7
Ryan N. Coffee <coffee@slac.stanford.edu>
2024-12-04 09:52:27 -0800

    Mucking with Argon Augers

--- utils/full_spect.py
+++ utils/full_spect.py
@@ -41,15 +41,19 @@ def main():
 
     fig,axs = plt.subplots(nrows=4, ncols=4, figsize=(25, 24))
     #startinds = [(v<<1)-(1<<10) for v in [24000, 24150, 24150, 24100, 24090, 23950, 23950, 24150, 24150, 24150, 25250, 24100, 24300, 24150, 24000, 24000 ]]
-    startinds = [3650, 4250, 4500, 4000, 4000, 3500, 3500, 4500, 4500, 4250, 8750, 4250, 4750, 4500, 3750, 3750]
+    #startinds = [3650, 4250, 4500, 4000, 4000, 3500, 3500, 4500, 4500, 4250, 8750, 4250, 4750, 4500, 3750, 3750]
+    #startinds = [1650, 2250, 2500, 2000, 2000, 1500, 1500, 2500, 2500, 2250, 6750, 2250, 2750, 2500, 1750, 1750]
+    #
+    # removing another 2000 for sake of _longAugers_new.h5
+    startinds = [-350, 250, 500, 000, 000, -500, -500, 500, 500, 250, 4750, 250, 750, 500, -250, -250]
     for i in range(len(startinds)):
         startinds[i] += (1<<16)+30000
         
-    window = [1500]*len(startinds)
+    window = [14000]*len(startinds)
     for i,p in enumerate(hist.keys()):
         col = i%4
         row = i>>2
-        axs[row,col].stairs(hist[p][startinds[i]:startinds[i]+window[i]],label=p)
+        axs[row,col].stairs(hist[p][startinds[i]:startinds[i]+window[i]],label='%s\n%i'%(p,startinds[i]))
         axs[row,col].legend()
         axs[row,col].set_xlabel('tof bins * inflate * expand')
         axs[row,col].set_ylabel('counts')
